3433515e321e442cdbc5b5cde7242f790f07fcf3
MacGiv <tomasluchelli.17@gmail.com>
2026-06-28 19:05:53 -0300

    Add dash state

---
+++ D2_Platformer_Canaza_Luchelli/Assets/Scripts/Player/States/PlayerDashState.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerDashState : PlayerState
+{
+    private float dashTime = 0.2f;
+    private float dashSpeed = 25f;
+    private float dashTimer;
+    private float prevGravityValue;
+
+    public PlayerDashState(PlayerBrain player, PlayerStateMachine stateMachine, string animBoolName)
+        : base(player, stateMachine, animBoolName) { }
+
+    public override void Enter()
+    {
+        base.Enter();
+        dashTimer = dashTime;
+        player.InputHandler.UseDashInput();
+
+        player.IsInvulnerable = true;
+        prevGravityValue = player.RB.gravityScale;
+        player.RB.gravityScale = 0f; // Avoid falling while airborne
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        player.MovementHandler.SetVelocity(dashSpeed * player.MovementHandler.FacingDirection, 0f);
+
+        dashTimer -= Time.deltaTime;
+
+        if (dashTimer <= 0)
+        {
+            stateMachine.ChangeState(player.CheckIfGrounded() ? player.IdleState : player.FallState);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        player.RB.gravityScale = prevGravityValue;
+        player.IsInvulnerable = false;
+    }
+}
