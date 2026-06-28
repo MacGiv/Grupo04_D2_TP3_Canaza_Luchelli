da59fba9f7e4a67a87dcaecbd2da5f3a4df3d5f0
MacGiv <tomasluchelli.17@gmail.com>
2026-06-28 19:06:20 -0300

    Update attack state

--- D2_Platformer_Canaza_Luchelli/Assets/Scripts/Player/States/PlayerAttackState.cs
+++ D2_Platformer_Canaza_Luchelli/Assets/Scripts/Player/States/PlayerAttackState.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
 /// <summary>
-/// State behavior for basic melee attack.
+/// State behavior for when the player executes a melee combo.
 /// </summary>
 public class PlayerAttackState : PlayerState
 {
+    private int comboCounter;
+    private bool comboInputBuffered;
     private bool isAnimationFinished;
 
     public PlayerAttackState(PlayerBrain player, PlayerStateMachine stateMachine, string animBoolName)
@@ -13,12 +15,11 @@ public class PlayerAttackState : PlayerState
     public override void Enter()
     {
         base.Enter();
+        player.MovementHandler.SetVelocity(0, 0);
+        comboCounter = 1;
+        comboInputBuffered = false;
         isAnimationFinished = false;
-
-        // Stop movement except for y velociti (mid-air)
-        player.MovementHandler.SetVelocity(0f, player.RB.linearVelocity.y);
-        // Consume attack
-        player.Anim.SetTrigger("attack");
+        PlayAttackAnimation();
         player.InputHandler.UseAttackInput();
     }
 
@@ -26,21 +27,47 @@ public class PlayerAttackState : PlayerState
     {
         base.LogicUpdate();
 
-        // Check state transition when animation is finished
+        //Input buffering
+        if (player.InputHandler.AttackInput)
+        {
+            comboInputBuffered = true;
+            player.InputHandler.UseAttackInput();
+        }
+
         if (isAnimationFinished)
         {
-            if (player.CheckIfGrounded())
-                stateMachine.ChangeState(player.IdleState);
+            if (comboInputBuffered && comboCounter < 3)
+            {
+                comboCounter++;
+                comboInputBuffered = false;
+                isAnimationFinished = false;
+
+                PlayAttackAnimation();
+            }
             else
-                stateMachine.ChangeState(player.FallState);
+            {
+                // If player didn't push attack or 3rd attack was already done
+                if (player.CheckIfGrounded())
+                    stateMachine.ChangeState(player.IdleState);
+                else
+                    stateMachine.ChangeState(player.FallState);
+            }
         }
     }
 
     /// <summary>
-    /// Called via Animation Event to poitn the state machine that the attack is over.
+    /// Updates the Animator with the current combo step and triggers the animation.
     /// </summary>
+    private void PlayAttackAnimation()
+    {
+        player.Anim.SetInteger("comboCounter", comboCounter);
+        player.Anim.SetTrigger("attack");
+    }
+
     public void FinishAttackAnimation()
     {
         isAnimationFinished = true;
     }
+    
+    public int GetComboCounter() => comboCounter;
 }
