e8a1e71c767440546bf81f6bb25d24331ea35ee5
MacGiv <tomasluchelli.17@gmail.com>
2026-06-29 02:25:27 -0300

    Update player settings

--- Assets/Scripts/PlayerSettingsSo.cs
+++ Assets/Scripts/PlayerSettingsSo.cs
@@ -6,13 +6,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Player Settings", menuName = "Settings/Player", order = 1)]
 public class PlayerSettingsSo : ScriptableObject
 {
+    [Header("General Settings")]
     public string playerName;
-    public int health = 100;
+
+    [Header("Movement Settings")]
     public int damage = 10;
     public float speed = 10;
     public float jumpForce = 15;
     public float coyoteTime = 0.15f;
+
+    [Header("Dash Settings")]
+    public float dashTime = 0.2f;
+    public float dashSpeed = 25;
+
+    [Header("Combat Settings")]
+    public int health = 100;
+    public float attackRadius = 0.5f;
     public float highScore = 0;
+    public  LayerMask whatIsEnemy;
+    public float knockbackForce = 1f;
 
     /// <summary>
     /// Initializes player settings loading saved data.
